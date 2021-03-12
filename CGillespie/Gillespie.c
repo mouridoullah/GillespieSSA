c5bf9fcc33bac42eaf067a92818aea917d320b3e
uvsq21901591 <55403324+uvsq21901591@users.noreply.github.com>
2021-03-12 22:03:18 +0100

    Update Gillespie.c

--- CGillespie/Gillespie.c
+++ CGillespie/Gillespie.c
@@ -17,40 +17,6 @@ void ecrireDansData(FILE *f, double t, double *x, int N){
     }
     fprintf(f, "\n");
 }
-/*--------------------------------------------------------*/
-void remplirTab(double t[], double n){
-	for (int i = 0; i < n; ++i){
-		printf("Donner la case numero %d : ", i+1);
-		scanf("%lf", &t[i]);
-	}
-}
-/*--------------------------------------------------------*/
-void afficherTab(double t[], double n){
-	int i;
-	for ( i = 0, printf("["); i < n; ++i){
-		printf("%f", t[i]);
-		if(i < n-1) printf(", ");
-	}
-	printf("]\n");
-}
-/*--------------------------------------------------------*/
-void remplirTab2D(double **t, int l, int c){
-	for (int i = 0; i < l; ++i){
-		printf("Veulliez remplir la %d la ligne : \n", i+1);
-		remplirTab(t[i], c);
-	}
-}
-/*--------------------------------------------------------*/
-void afficherTab2D(double** t, int l, int c){
-	for (int i = 0; i < l; ++i){
-		afficherTab(t[i], c);
-		//printf("\n");
-	}
-}
-/*--------------------------------------------------------*/
-void allouerTableau(double** t, int n){
-	*t = (double*)malloc(n*sizeof(double));
-}
 /*-------------------------------------------------------------*/
 double* calculDeH(double* x, int n) {
 	double* h = malloc(n*sizeof(double));
@@ -99,7 +65,7 @@ void miseAJourDesX(double* x, double v[][NOMBRES_ESPECES], int mu, int N) {
 /*-------------------------------------------------------------*/
 void Gillespie(char *myfile, double* c, double v[][NOMBRES_ESPECES], double* x, int N, int M, double T) {
 	FILE *data;
-    data = fopen(myfile, "w");
+        data = fopen(myfile, "w");
 	int mu = 0;
 	double r1 = 0, r2 = 0, a0 = 0, t = 0;
 	double* h;
@@ -137,45 +103,9 @@ void Gillespie(char *myfile, double* c, double v[][NOMBRES_ESPECES], double* x, 
 int main(int argc, char const *argv[]){
 	double c[] = {5000.0, 50.0, 0.00005, 5.0};
 	double x[] = {1000, 2000};
-	double v[NOMBRES_DE_REACTIONS][NOMBRES_ESPECES] = 	{ {1, 0},
-												 	     {-1, 1},
-												 	     {1, -1},
-												 	     {-1, 0} };
-
-
+	double v[NOMBRES_DE_REACTIONS][NOMBRES_ESPECES] = { {1, 0}, {-1, 1}, {1, -1}, {-1, 0} };
 	
 	Gillespie("data", c, v, x, NOMBRES_ESPECES, NOMBRES_DE_REACTIONS, TEMPS_MAX);
 
-
-	/*--------------------------------------------------*/
-	// double x[] = {500,2000};
-	// double c[] = {5000.0, 50.0, 0.00005, 5.0};
-	// afficherTab(x,NOMBRES_ESPECES);
-
-	// double* h = calculDeH(x, NOMBRES_DE_REACTIONS);
-	// afficherTab(h,NOMBRES_DE_REACTIONS);
-
-
-	// double* a = calculDePropensity(h,c,NOMBRES_DE_REACTIONS);
-	// afficherTab(a,NOMBRES_DE_REACTIONS);
-
-	// double a0 = sommeDesA(a,NOMBRES_DE_REACTIONS);
-	// printf("%f\n", a0);
-
-	// unsigned int iseed = (unsigned int)time(NULL);
-	// srand (iseed);
-	// double r2 = ((double)rand()/(double)RAND_MAX);
-	// int mu = calculDMu(a, r2, NOMBRES_DE_REACTIONS);
-	// printf("%d\n", mu);
-
-
-	// //afficherTab2D((double**)v, M, N);
-	// //afficherTab((double*)v[0], N);
-
-	// miseAJourDesX(x,v,mu,NOMBRES_ESPECES);
-	// afficherTab(x,NOMBRES_ESPECES);
-
-	// free(h);
-	// free(a);
 	return 0;
 }
