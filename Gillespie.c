bb584a74fbc852d57dcccd17924da3c0927b4dd0
mouridoullah <bdame711@gmail.com>
2021-03-13 19:03:39 +0100

    Version C

--- Gillespie.c
+++ Gillespie.c
@@ -6,6 +6,7 @@
 
 #define NOMBRES_ESPECES 3
 #define NOMBRES_DE_REACTIONS 9
+#define TEMPS_MAX 0.0002
 
 /*--------------------------------------------------------*/
 void ecrireDansData(FILE *f, double t, double *x, int N){
@@ -16,40 +17,6 @@ void ecrireDansData(FILE *f, double t, double *x, int N){
     }
     fprintf(f, "\n");
 }
-/*--------------------------------------------------------*/
-void remplirTab(double t[], double n){
-	for (int i = 0; i < n; ++i){
-		printf("Donner la case numero %d : ", i+1);
-		scanf("%lf", &t[i]);
-	}
-}
-/*--------------------------------------------------------*/
-void afficherTab(double t[], double n){
-	int i;
-	for ( i = 0, printf("["); i < n; ++i){
-		printf("%f", t[i]);
-		if(i < n-1) printf(", ");
-	}
-	printf("]\n");
-}
-/*--------------------------------------------------------*/
-void remplirTab2D(double **t, int l, int c){
-	for (int i = 0; i < l; ++i){
-		printf("Veulliez remplir la %d la ligne : \n", i+1);
-		remplirTab(t[i], c);
-	}
-}
-/*--------------------------------------------------------*/
-void afficherTab2D(double** t, int l, int c){
-	for (int i = 0; i < l; ++i){
-		afficherTab(t[i], c);
-		//printf("\n");
-	}
-}
-/*--------------------------------------------------------*/
-void allouerTableau(double** t, int n){
-	*t = (double*)malloc(n*sizeof(double));
-}
 /*-------------------------------------------------------------*/
 double genererNombreAlea(){
 	return (double)rand()/RAND_MAX;
@@ -57,10 +24,6 @@ double genererNombreAlea(){
 /*-------------------------------------------------------------*/
 double* calculDeH(double* x, int n) {
 	double* h = malloc(n*sizeof(double));
-    // h[0] = 1.0;
-    // h[1] = x[0];
-    // h[2] = 0.5*x[0]*x[1]*(x[0] - 1);
-    // h[3] = x[0];
     h[0] = 1.0;
     h[1] = 1.0;
     h[2] = 1.0;
@@ -121,12 +84,6 @@ void Gillespie(char *myfile, double* c, double v[][NOMBRES_ESPECES], double* x, 
 
 	while(t < T) {
 
-		// printf("%f ", t);
-	 //    for(int i = 0; i < N; ++i){
-	 //    	printf("%f ", x[i]);
-	 //    }
-		// printf("\n");
-
 		h = calculDeH(x, M);
 		a = calculDePropensity(h, c, M);
 		a0 = sommeDesA(a, M);
@@ -147,18 +104,9 @@ void Gillespie(char *myfile, double* c, double v[][NOMBRES_ESPECES], double* x, 
 }
 
 int main(int argc, char const *argv[]){
-	double T = 50.20;
+
 	double c[] = {5000,5000,5000,5,5,5,0.00001,0.00001,0.00001};
 	double x[] = {1000, 1000, 1000};
-	// double v[NOMBRES_DE_REACTIONS][NOMBRES_ESPECES] = 	{ {1, 0, -1},
-	// 											 	      {-1, 1, 0},
-	// 											 	      {1, -1, 0},
-	// 											 	      {-1, 0, 1},
-	// 											 	      {1, 0, -1},
-	// 											 	      {0, -1, 1},
-	// 											 	      {-1, 1, 0},
-	// 											 	      {1, 0, -1},
-	// 											 	      {0, -1, 1} };
 
 	double v[NOMBRES_DE_REACTIONS][NOMBRES_ESPECES] = 	{ {0,1,-1},
 											 	     	  {-1,0,1},
@@ -169,41 +117,8 @@ int main(int argc, char const *argv[]){
 											 	      	  {-1,0,1},
 											 	      	  {1,-1,0},
 											 	      	  {0,1,-1} };
-
-
 	
-	Gillespie("data.txt", c, v, x, NOMBRES_ESPECES, NOMBRES_DE_REACTIONS, T);
+	Gillespie("data.txt", c, v, x, NOMBRES_ESPECES, NOMBRES_DE_REACTIONS, TEMPS_MAX);
 
-
-	/*--------------------------------------------------*/
-	// double x[] = {500,2000};
-	// double c[] = {5000.0, 50.0, 0.00005, 5.0};
-	// afficherTab(x,NOMBRES_ESPECES);
-
-	// double* h = calculDeH(x, NOMBRES_DE_REACTIONS);
-	// afficherTab(h,NOMBRES_DE_REACTIONS);
-
-
-	// double* a = calculDePropensity(h,c,NOMBRES_DE_REACTIONS);
-	// afficherTab(a,NOMBRES_DE_REACTIONS);
-
-	// double a0 = sommeDesA(a,NOMBRES_DE_REACTIONS);
-	// printf("%f\n", a0);
-
-
-	// srand(time(NULL));
-	// double r2 = genererNombreAlea();
-	// int mu = calculDMu(a, r2, NOMBRES_DE_REACTIONS);
-	// printf("%d\n", mu);
-
-
-	// // //afficherTab2D((double**)v, M, N);
-	// // //afficherTab((double*)v[0], N);
-
-	// miseAJourDesX(x,v,mu,NOMBRES_ESPECES);
-	// afficherTab(x,NOMBRES_ESPECES);
-
-	// free(h);
-	// free(a);
 	return 0;
 }
